ab31bba9ddfa2aba466fa39c6157f318193ffa69
kgoodman <kellangoodman@gmail.com>
2023-01-31 19:43:14 -0800

    adding pages

--- client/src/components/personalInfo.jsx
+++ client/src/components/personalInfo.jsx
@@ -7,18 +7,6 @@ function PersonalInfo() {
 
   const {id} = useParams()
   const [user, setuser] = useState({})
-
-  useEffect(()=>{
-    axios.get(`http://localhost:8000/api/oneuser/${id}`)
-    .then((res)=>{
-      setuser(res.data)
-      console.log(res);
-    }).catch((err)=>{
-      console.log(err)
-
-    });
-  },[]);
-
   const navigate = useNavigate()
   const [errors, setErrors] = useState({})
   const [devPicture, setdevPicture] = useState("");
@@ -27,11 +15,33 @@ function PersonalInfo() {
   const [Fname, setFname] = useState("");
   const [Lname, setLname] = useState("");
 
-  const submitFname = (e) => {
+  useEffect(()=>{
+    axios.get(`http://localhost:8000/api/oneuser/${id}`)
+    .then((res)=>{
+      setuser(res.data)
+      setFname(res.data.Fname)
+      setLname(res.data.Lname)
+      setdevPicture(res.data.devPicture)
+      setdevSummary(res.data.devSummary)
+      setdevTools(res.data.devTools)
+      console.log(res);
+    }).catch((err)=>{
+      console.log(err)
+
+    });
+  },[]);
+
+
+
+  const submitLname = (e) => {
     e.preventDefault();
     axios
       .put(`http://localhost:8000/api/updateuser/${id}`, {
-        Fname
+        Fname,
+        Lname,
+        devPicture,
+        devSummary,
+        devTools
       }, { withCredentials: true })
       .then((res) => {
         console.log(res);
@@ -47,71 +57,6 @@ function PersonalInfo() {
     navigate('/')
   }
 
-  const submitLname = (e) => {
-    e.preventDefault();
-    axios
-      .put(`http://localhost:8000/api/updateuser/${id}`, {
-        Lname
-      }, { withCredentials: true })
-      .then((res) => {
-        console.log(res);
-        navigate(`/user/${id}`)
-      })
-      .catch((err) => {
-        console.log(err)
-        setErrors(err)
-      });
-  };
-
-  const submitpicture = (e) => {
-    e.preventDefault();
-    axios
-      .put(`http://localhost:8000/api/updateuser/${id}`, {
-        devPicture
-      }, { withCredentials: true })
-      .then((res) => {
-        console.log(res);
-        navigate(`/user/${id}`)
-      })
-      .catch((err) => {
-        console.log(err)
-        setErrors(err)
-      });
-  };
-
-  const submitsummary = (e) => {
-    e.preventDefault();
-    axios
-      .put(`http://localhost:8000/api/updateuser/${id}`, {
-        devSummary
-      }, { withCredentials: true })
-      .then((res) => {
-        console.log(res);
-        navigate(`/user/${id}`)
-      })
-      .catch((err) => {
-        console.log(err)
-        setErrors(err)
-      });
-  };
-
-  const submittools = (e) => {
-    e.preventDefault();
-    axios
-      .put(`http://localhost:8000/api/updateuser/${id}`, {
-
-        devTools  
-      }, { withCredentials: true })
-      .then((res) => {
-        console.log(res);
-        navigate(`/user/${id}`)
-      })
-      .catch((err) => {
-        console.log(err)
-        setErrors(err)
-      });
-  };
-
   
 
   return (
@@ -120,7 +65,7 @@ function PersonalInfo() {
     <button onClick={homebtn}>Home</button>
     <div>
     <div>Welcome, {user.Fname}</div>
-    <form onSubmit={submitFname}>
+    <form onSubmit={submitLname}>
     <div>
       First Name: <input onChange={(e) => setFname(e.target.value)}
                 value={Fname}
@@ -128,10 +73,8 @@ function PersonalInfo() {
                 type="text" 
                 placeholder={user.Fname}/>
                 {errors.Fname ? <p className="validations alert" style={{color: 'red'}}>{errors.Fname.message}</p> : null}
-      <button type='submit'>Update</button>
+                <button onClick={submitLname}> Update</button>
       </div>
-      </form>
-      <form onSubmit={submitLname}>
       <div>
       Last Name: <input onChange={(e) => setLname(e.target.value)}
                 value={Lname}
@@ -139,10 +82,7 @@ function PersonalInfo() {
                 type="text" 
                 placeholder={user.Lname}/>
                 {errors.Lname ? <p className="validations alert" style={{color: 'red'}}>{errors.Lname.message}</p> : null}
-      <button type='submit'>Update</button>
       </div>
-      </form>
-      <form onSubmit={submitpicture}>
       <div>
       Picture: <input onChange={(e) => setdevPicture(e.target.value)}
                 value={devPicture}
@@ -150,10 +90,7 @@ function PersonalInfo() {
                 type="text"
                 accept='image/'/>
                 {errors.devPicture ? <p className="validations alert" style={{color: 'red'}}>{errors.devPicture.message}</p> : null}
-                <button type='submit'>Update</button>
       </div>
-      </form>
-      <form onSubmit={submitsummary}>
       <div>
       Summary: <textarea onChange={(e) => setdevSummary(e.target.value)}
                 value={devSummary}
@@ -162,17 +99,13 @@ function PersonalInfo() {
                 rows={4}
                 placeholder={user.devSummary}/>
                 {errors.Lname ? <p className="validations alert" style={{color: 'red'}}>{errors.Lname.message}</p> : null}
-                <button type='submit'>Update</button>
       </div>
-      </form>
-      <form onSubmit={submittools}>
       <div>
       Tools: <input onChange={(e) => setdevTools(e.target.value)}
                 value={devTools}
                 name="devTools"
                 type="text"/>
                 {errors.devTools ? <p className="validations alert" style={{color: 'red'}}>{errors.devTools.message}</p> : null}
-                <button type='submit'>Update</button>
       </div>
       </form>
 
